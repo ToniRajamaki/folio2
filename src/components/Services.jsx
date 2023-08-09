49ee555c6c68a8876b922568f6d9922493246b23
TONI <zingoo16@gmail.com>
2023-08-09 18:37:02 +0300

    started working on services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -19,74 +19,14 @@ useEffect(() => {
         <h2 className="section__title">Services</h2>
         <span className="section__subtitle">What i offer</span>
         <div className="services__container container grid">
-          <div className="services__content">
-            <div>
-              <i className="uil uil-web-grid services__icon"></i>
-              <h3 className="services__title">
-                Web <br />
-                Design
-              </h3>
-            </div>
-            <span className="services__button" onClick={() => toggleTab(1)}>
-              View More
-              <i className="uil uil-arrow-right services__button-icon"></i>
-            </span>
-            <div
-              className={
-                toggleState === 1
-                  ? "services__modal active-modal"
-                  : "services__modal"
-              }
-            >
-              <div className="services__modal-content">
-                <i
-                  onClick={() => toggleTab(0)}
-                  className="uil uil-times services__modal-close"
-                ></i>
-                <h3 className="services__modal-title">Web Design</h3>
-                {/* <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
-              </p> */}
-
-                <ul className="services__modal-services grid">
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle service__modal-icon"></i>
-                    <p className="services__modal-info">
-                      I develop the user interface.
-                    </p>
-                  </li>
-
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle service__modal-icon"></i>
-                    <p className="services__modal-info"> Site purpose.</p>
-                  </li>
-
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle service__modal-icon"></i>
-                    <p className="services__modal-info">Site objectives.</p>
-                  </li>
-
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle service__modal-icon"></i>
-                    <p className="services__modal-info">Fresh content.</p>
-                  </li>
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle service__modal-icon"></i>
-                    <p className="services__modal-info">Accessibility.</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
+          {/* ######################################################################################## */}
           <div className="services__content">
             <div>
               <i className="uil uil-arrow services__icon"></i>
               <h3 className="services__title">
                 {" "}
-                web <br />
-                Front-end
+                Web <br />
+                Development
               </h3>
             </div>
             <span onClick={() => toggleTab(2)} className="services__button">
@@ -146,13 +86,74 @@ useEffect(() => {
               </div>
             </div>
           </div>
+{/* ######################################################################################## */}
+<div className="services__content">
+            <div>
+              <i className="uil uil-web-grid services__icon"></i>
+              <h3 className="services__title">
+                UI/UX <br />
+                Design
+              </h3>
+            </div>
+            <span className="services__button" onClick={() => toggleTab(1)}>
+              View More
+              <i className="uil uil-arrow-right services__button-icon"></i>
+            </span>
+            <div
+              className={
+                toggleState === 1
+                  ? "services__modal active-modal"
+                  : "services__modal"
+              }
+            >
+              <div className="services__modal-content">
+                <i
+                  onClick={() => toggleTab(0)}
+                  className="uil uil-times services__modal-close"
+                ></i>
+                <h3 className="services__modal-title">Web Design</h3>
+                {/* <p className="services__modal-description">
+                Service with more than 3 years of experience.Providing quality
+                work to clients and companies.
+              </p> */}
 
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle service__modal-icon"></i>
+                    <p className="services__modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle service__modal-icon"></i>
+                    <p className="services__modal-info"> Site purpose.</p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle service__modal-icon"></i>
+                    <p className="services__modal-info">Site objectives.</p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle service__modal-icon"></i>
+                    <p className="services__modal-info">Fresh content.</p>
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle service__modal-icon"></i>
+                    <p className="services__modal-info">Accessibility.</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+{/* ######################################################################################## */}
           <div className="services__content">
             <div>
               <i className="uil uil-edit services__icon"></i>
               <h3 className="services__title">
-                Web <br />
-                Back-End
+                Software <br />
+                Development
               </h3>
             </div>
             <span onClick={() => toggleTab(3)} className="services__button">
