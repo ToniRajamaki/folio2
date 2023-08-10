383335a5f77da9f4a5bcfc2330875b9e485433c0
TONI <zingoo16@gmail.com>
2023-08-10 09:57:07 +0300

    finished education part with links and some textual decoration

--- src/components/Education.js
+++ src/components/Education.js
@@ -33,33 +33,43 @@ function Education() {
           <h2 className='section__title_white'>Education</h2>
           <span className='section__subtitle_light'>My Degrees</span>
           <div className='cards-container container grid2' data-aos='fade-up'>
-            <div className='card'>
-              <EducationCard
-                schoolName='Tampere University'
-                degree='Human-Technology Interaction (MSc Tech)'
-                duration='2018-2023'
-                description='Long Major: Human-Technology Interaction'
-                imageSrc='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyOseqgHwSg_KeYaOXu4-xQr_ZDTF98GJmTg&usqp=CAU'
-              />
-            </div>
-            <div className='card'>
-              <EducationCard
-                schoolName='AhlmanEdu'
-                degree='Professional Entrepreneurship Diploma'
-                duration='2019 - 2022'
-                description='Development of business operations Productization, Sales & Marketing'
-                imageSrc={`url('./folio2/assets/toni/tau.png')`}
-              />
-            </div>
-            <div className='card'>
-              <EducationCard
-                schoolName='Tampere University'
-                degree='Bachelor s Degree in Computer Science'
-                duration='2018-2022'
-                description='Major: Signal Processing | Minor: Industrial Economics'
-                imageSrc='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyOseqgHwSg_KeYaOXu4-xQr_ZDTF98GJmTg&usqp=CAU'
-              />
-            </div>
+            <a href='https://www.tuni.fi/en/study-with-us/human-technology-interaction-computing-sciences'>
+              <div className='card'>
+                <EducationCard
+                  schoolName='Tampere University'
+                  degree={`Human-Technology Interaction
+                  Master's Degree`}
+                  duration='2021-2023'
+                  description='• Long Major: Human-Technology Interaction'
+                  imageSrc='./folio2/assets/toni/tau_logo.png'
+                />
+              </div>
+            </a>
+            <a href='https://ahlmanedu.fi/'>
+              <div className='card'>
+                <EducationCard
+                  schoolName='AhlmanEdu'
+                  degree='Professional Entrepreneurship Diploma'
+                  duration='2021 - 2022'
+                  description={`• Business operations development
+                • Productization, Sales & Marketing`}
+                  imageSrc='./folio2/assets/toni/ahlman.png'
+                />
+              </div>
+            </a>
+            <a href='https://www.tuni.fi/en/about-us/computing-sciences#switcher-trigger-overview'>
+              <div className='card'>
+                <EducationCard
+                  schoolName='Tampere University'
+                  degree={`Computing Sciences
+                  Bachelor's Degree`}
+                  duration='2018-2021'
+                  description={`• Major: Signal Processing
+                • Minor: Industrial Economics`}
+                  imageSrc='./folio2/assets/toni/tau_logo.png'
+                />
+              </div>
+            </a>
           </div>
         </section>
       </div>
