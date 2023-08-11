5982c6999d8db7bc8f7c1b10b0c5d561b68edac0
TONI <zingoo16@gmail.com>
2023-08-11 10:59:08 +0300

    good progress in skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,6 +7,8 @@ import "../styles/work.css"
 import Works from './Works'
 const Skills = ({ theme }) => {
   const [activeTab, setActiveTab] = useState('coding');
+  const code_icon = <i className='uil uil-arrow icon' style={{ color: "#516CF7" }}></i>;
+  const ui_icon = <i className='bx bxs-palette icon2' style={{ color: "#9251F7" }}></i>;
 
   useEffect(() => {
     AOS.init({ duration: 2000 });
@@ -39,21 +41,24 @@ const Skills = ({ theme }) => {
               <TagComponent
                 tags={["JavaScript", "React", "Redux", "Dart", "CSS"]}
                 heading='Front-end'
+                icon={code_icon}
               ></TagComponent>
             </div>
             <div className="skills__content">
               <TagComponent
                 tags={["C#", "C++", "C", "Python", "Java", "Scala", "Matlab", "Assembly"]}
                 heading='Back-end'
+                icon={code_icon}
               ></TagComponent>
             </div>
-            {/* <div className='tag_container_narrow' */}
+            {/* <div className='tag_container' */}
       {/* data-aos="fade-up"> */}
         <div className="skills__content">
 
           <TagComponent
             tags={["Unity","TailWindCSS","Scikit-Learn","Tensorflow","Numpy","OpenCV","Apache Spark","Git","SourceTree","Pandas","Unity","Unreal Engine","Database-lib","Axios"]}
             heading='Frameworks etc' 
+            icon={code_icon}
             ></TagComponent>
             </div>
 
@@ -61,12 +66,13 @@ const Skills = ({ theme }) => {
         )}
 
         {activeTab === 'uiux' && (
-          <div className='tag_container_narrow'>
+          <div className='tag_container'>
             <div className="skills__content_purple">
               <TagComponent
-                tags={["UI Design", "Visual Design", "UX Design", "Testing", "Interaction Design", "Accessability", "Prototyping", "Web/Mobile Design", "Agile Development", "Experience Driven Design", "AR/VR Design", "User Research", "Wireframing", "Human-Centered Design"]}
+                tags={["UI Design", "Visual Design", "UX Design", "Testing", "Interaction Design", "Accessability", "Prototyping", "Web/Mobile Design", "Agile Development", "AR/VR Design", "User Research", "Wireframing", "Human-Centered Design", "Experience Driven Design"]}
                 heading='UI/UX Skills'
                 color="#9251F7"
+                icon={ui_icon}
               ></TagComponent>
             </div>  
           <div className="skills__content_purple">
@@ -75,6 +81,7 @@ const Skills = ({ theme }) => {
           tags={["Figma","Photoshop","Penbot","Sketch","Adobe XD","Proto.io","Canva","Miro","Diagram.io","Mural","Trello"]}
           heading='UI/UX Tools' 
           color="#9251F7"
+          icon={ui_icon}
           ></TagComponent>
           </div>
           </div>
--- src/components/TagComponent.js
+++ src/components/TagComponent.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import '../styles/TagComponent.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../styles/TagComponent.css'
 
-const TagComponent = ({ tags, heading, color }) => {
+const TagComponent = ({ tags, heading, color,icon }) => {
   const tagStyle = {
     color: color,
     borderColor: color,
-  };
+  }
 
   return (
     <div>
-      <h2 style={tagStyle}>{heading}</h2>
+      <div className='pair'>
+        {/* <i class='bx bxs-palette icon2' style={tagStyle}> </i> */}
+        {icon}
+        <h2 style={tagStyle}>{heading}</h2>
+      </div>
+
       <div className='tags'>
         {tags.map((tag, index) => (
           <span key={index} className='tag tag-design-12' style={tagStyle}>
@@ -19,17 +24,17 @@ const TagComponent = ({ tags, heading, color }) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
 TagComponent.propTypes = {
   tags: PropTypes.arrayOf(PropTypes.string).isRequired,
   heading: PropTypes.string.isRequired,
   color: PropTypes.string, // New prop for text and border color
-};
+}
 
 TagComponent.defaultProps = {
   color: '#516CF7', // Default value for color prop
-};
+}
 
-export default TagComponent;
+export default TagComponent
