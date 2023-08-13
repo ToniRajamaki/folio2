e366ddc62e81e0db1618e83965aa990a05818b19
TONI <zingoo16@gmail.com>
2023-08-13 12:37:44 +0300

    good progress with qualifications section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -24,32 +24,94 @@ function Experience() {
       date: 'Jan 2021 - Dec 2022',
       icon: 'work',
       imageSrc: './folio2/assets/toni/tau_logo.png',
-    }, {
-      id: 1,
-      title: 'Game Software Developer',
-      location: 'Tampere, Finland',
+      workPlace:'Git Gud Games®'
+    },
+    {
+      id: 2,
+      title: 'Meat Processing Laborer',
+      location: 'Seinäjoki, Finland',
       description: [
-        'Mobile game development using Unity game engine and C#.',
-        'Implement and optimize game assets for performance on mobile devices.',
-        'Collaborate with cross functional teams, including artists and programmers.',
+        'Prepare and process meat products according to standard procedures',
+        'Operate machinery and knife for cutting, grinding and processing meat.',
       ],
       buttonText: 'View Frontend Projects',
-      date: 'Jan 2021 - Dec 2022',
+      date: 'May 2019 - Jun 2019',
       icon: 'work',
       imageSrc: './folio2/assets/toni/tau_logo.png',
-    }, {
-      id: 1,
-      title: 'Game Software Developer',
-      location: 'Tampere, Finland',
+      workPlace:'Atria®'
+    },
+    {
+      id: 3,
+      title: 'Technology Sales Specialist',
+      location: 'Kauhava, Finland',
       description: [
-        'Mobile game development using Unity game engine and C#.',
-        'Implement and optimize game assets for performance on mobile devices.',
-        'Collaborate with cross functional teams, including artists and programmers.',
+        'Work autonomously, responsible for all store tasks and attending to customers',
+        'Repair and troubleshoot electornics such as laptops for customers.',
+        'Coordinate with suppliers and vendors to ensure product availability.',
       ],
       buttonText: 'View Frontend Projects',
-      date: 'Jan 2021 - Dec 2022',
+      date: 'May 2018 - Aug 2018',
       icon: 'work',
       imageSrc: './folio2/assets/toni/tau_logo.png',
+      workPlace:'Datakaista®'
+    },
+    {
+      id: 4,
+      title: 'Custom Furniture Fabricator',
+      location: 'Kauhava,Finland',
+      description: [
+        'Coordinate subcontracting tasks related to furniture manufacturing.',
+        'Ensure quality control and adherence to manufacturing standards.',
+        'Provide guidance and supervision to individuals in rehabilitative employment.',
+      ],
+      buttonText: 'View Frontend Projects',
+      date: 'Jul 2016 - Jul 2017',
+      icon: 'work',
+      imageSrc: './folio2/assets/toni/tau_logo.png',
+      workPlace:'Unico®'
+    },
+    {
+      id: 5,
+      title: 'Park Maintenance Worker',
+      location: 'Kauhava, Finland',
+      description: [
+        'Guide and supervise other summer workers in their tasks.',
+        'Provide training and instructions to other seasonal workers.',
+      ],
+      buttonText: 'View Frontend Projects',
+      date: 'Jun 2015 - Aug 2015',
+      icon: 'work',
+      imageSrc: './folio2/assets/toni/tau_logo.png',
+      workPlace:'City of Kauhava'
+    },
+    {
+      id: 6,
+      title: 'Park Maintenance Worker',
+      location: 'Kauhava, Finland',
+      description: [
+        'Ensure the upkeep and cleanliness of shared outdoor spaces.',
+        'Collaborate with management to address maintenance needs and priorities.',
+      
+      ],
+      buttonText: 'View Frontend Projects',
+      date: 'Jun 2014 - Aug 2014',
+      icon: 'work',
+      imageSrc: './folio2/assets/toni/tau_logo.png',
+      workPlace:'City of Kauhava'
+    },
+    {
+      id: 7,
+      title: 'Construction Worker',
+      location: 'Seinäjoki, Finland',
+      description: [
+        'Perform refurbishment and renovation work on buildings.',
+        'Work included tasks such as: plastering, masonry and painting.',
+      ],
+      buttonText: 'View Frontend Projects',
+      date: 'Jun 2013 - Jul 2013',
+      icon: 'work',
+      imageSrc: './folio2/assets/toni/tau_logo.png',
+      workPlace:'Rakennusliike Virrankoski®'
     },
   ]
   //   let workIconStyles = { background: "#06D6A0" };
@@ -66,7 +128,13 @@ function Experience() {
               date={element.date}
               dateClassName='date'
               icon={checkIcon}
-              iconStyle={{background:'rgb(33,150,243)',width:"50px",display:"flex",justifyContent:"center",alignItems:"center"}}
+              iconStyle={{
+                background: 'rgb(33,150,243)',
+                width: '50px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
             >
               <div className='jobcard'>
                 <div className='parent'>
@@ -75,24 +143,23 @@ function Experience() {
                       alt='jobImage'
                       className='work_image'
                       src={element.imageSrc}
-                    ></img>
+                    />
                     <div className='title_and_workplace'>
+                      <h3 className='work_title_header_font'>{element.workPlace}</h3>
                       <h5 className='work_title_font'>{element.title}</h5>
-                      <h3 className='work_title_header_font'>Git Gud Games</h3>
+                    </div>
+                    <div className='location'>
+                      {locationIcon} {element.location}
                     </div>
                   </div>
-                  <span className='location'>
-                    {locationIcon} {element.location}
-                  </span>
                 </div>
-
-                <div className='description2'>
-                  {element.description.map((desc, index) => (
-                    <p key={index} className='tiny_font parent2'>
-                      <div className='space_dot'>• </div> {desc}
-                    </p>
-                  ))}
-                </div>
+              </div>
+              <div className='description2'>
+                {element.description.map((desc, index) => (
+                  <p key={index} className='tiny_font parent2'>
+                    <div className='space_dot'>• </div> {desc}
+                  </p>
+                ))}
               </div>
             </VerticalTimelineElement>
           )
