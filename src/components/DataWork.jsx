af1dfcbb3cdb544bcda46a148d376c5e1325f625
TONI <zingoo16@gmail.com>
2023-08-19 13:22:34 +0300

    Added A LOT of content for projects

--- src/components/DataWork.jsx
+++ src/components/DataWork.jsx
@@ -9,8 +9,8 @@ export const projectsData = [
   },
   {
     id: 2,
-    title: "Tetris C++",
-    images: ["./folio2/assets/toni/logo.png"],
+    title: "Tetris+",
+    images: ["./folio2/assets/projects/misc/tetris.png"],
     category: "App",
     video: "/assets/camping.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
@@ -19,7 +19,7 @@ export const projectsData = [
     id: 3,
     title: "FUZU",
     images: ["./folio2/assets/projects/fuzu/fuzu1.png",
-    // "./folio2/assets/projects/fuzu/fuzu2.mp4",
+    "./folio2/assets/projects/fuzu/fuzu2.gif",
     "./folio2/assets/projects/fuzu/fuzu3.jpg",
     "./folio2/assets/projects/fuzu/fuzu4.jpg",
     "./folio2/assets/projects/fuzu/fuzu5.jpg",
@@ -31,7 +31,7 @@ export const projectsData = [
   {
     id: 4,
     title: "Pirkanmaa Domination",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/misc/pirkanmaan_valloitus.png",],
     category: "App",
     video: "/assets/gym.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
@@ -39,7 +39,7 @@ export const projectsData = [
   {
     id: 5,
     title: "Suez Unblocker",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/suez/suez1.png","./folio2/assets/projects/suez/suez2.png","./folio2/assets/projects/suez/suez3.png"],
     category: "App",
     video: "/assets/movie.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
@@ -47,7 +47,12 @@ export const projectsData = [
   {
     id: 6,
     title: "Cyberpunk: Rogue Turret",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/cyberpunk/cyber1.png",
+    "./folio2/assets/projects/cyberpunk/cyber2.png",
+    "./folio2/assets/projects/cyberpunk/cyber3.png",
+    "./folio2/assets/projects/cyberpunk/cyber4.png",
+    "./folio2/assets/projects/cyberpunk/cyber5.png",
+  ],
     category: "App",
     video: "/assets/todo.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
@@ -55,22 +60,27 @@ export const projectsData = [
 
   {
     id: 7,
-    title: "Juice Stacker",
-    images: ["./folio2/assets/toni/logo.png"],
+    title: "Juice Master",
+    images: ["./folio2/assets/projects/juice/juice0.png",
+    "./folio2/assets/projects/juice/juice1.png",
+    "./folio2/assets/projects/juice/juice2.png",
+    "./folio2/assets/projects/juice/juice3.png",
+    "./folio2/assets/projects/juice/juice4.png",
+  ],
     category: "app",
     video: "/assets/gym_mobile.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
   {
-    id: 7,
+    id: 8,
     title: "Tampere Bus Algorithms",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/misc/bus.png"],
     category: "app",
     video: "/assets/gym_mobile.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
   {
-    id: 7,
+    id: 9,
     title: "Sword Stacker",
     images: ["./folio2/assets/toni/logo.png"],
     category: "app",
@@ -78,45 +88,50 @@ export const projectsData = [
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
   {
-    id: 7,
+    id: 10,
     title: "CIFAR-10 Image Recognition",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/cifar10/cifar2.png","./folio2/assets/projects/cifar10/cifar1.jpg"],
     category: "app",
     video: "/assets/gym_mobile.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
   {
-    id: 7,
+    id: 11,
     title: "AR/VR Augmented Food",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/ar/ar0.png",
+    "./folio2/assets/projects/ar/ar1.gif",
+    "./folio2/assets/projects/ar/ar2.png",
+    "./folio2/assets/projects/ar/ar3.png",
+  ],
     category: "app",
     video: "/assets/gym_mobile.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
   {
-    id: 7,
+    id: 12,
     title: "Ninja Blog",
-    images: ["./folio2/assets/toni/logo.png"],
+    images: ["./folio2/assets/projects/ninja_blog/blog0.png",
+    "./folio2/assets/projects/ninja_blog/blog1.gif",
+    "./folio2/assets/projects/ninja_blog/blog2.gif"],
     category: "web",
     video: "/assets/gym_mobile.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
   {
-    id: 7,
-    title: "Tampere Bus Algorithms",
-    images: ["./folio2/assets/toni/logo.png"],
-    category: "app",
+    id: 12,
+    title: "Mini Web project collection",
+    images: [
+      "./folio2/assets/projects/mini_projects/ez1.png",
+      "./folio2/assets/projects/mini_projects/ez2.gif",
+      "./folio2/assets/projects/mini_projects/ez3.gif",
+      "./folio2/assets/projects/mini_projects/ez4.gif",
+      "./folio2/assets/projects/mini_projects/ez5.gif",
+    ],
+    category: "web",
     video: "/assets/gym_mobile.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
   },
-  {
-    id: 7,
-    title: "Tampere Bus Algorithms",
-    images: ["./folio2/assets/toni/logo.png"],
-    category: "app",
-    video: "/assets/gym_mobile.mp4",
-    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say."
-  },
+
 ];
 
 export const projectsNav = [
