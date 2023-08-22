8e46f63120e4171feef709eea867aba125433c5d
TONI <zingoo16@gmail.com>
2023-08-22 09:48:27 +0300

    projects now have proper categories

--- src/components/DataWork.jsx
+++ src/components/DataWork.jsx
@@ -1,152 +1,155 @@
 
 export const projectsData = [
   {
+    id: 11,
+    title: "AR/VR Augmented Food",
+    images: ["./folio2/assets/projects/ar/ar0.png",
+    "./folio2/assets/projects/ar/ar1.gif",
+    "./folio2/assets/projects/ar/ar2.png",
+    "./folio2/assets/projects/ar/ar3.png",
+  ],
+    category: ["ml & ai","featured"],
+    video: "/assets/gym_mobile.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["C#","VR","AR"],
+    features:["Augment Food","Real-Time Application","Calibration & Testing"]
+  },
+  {
+    id: 10,
+    title: "Image Recognition",
+    images: ["./folio2/assets/projects/cifar10/cifar2.png","./folio2/assets/projects/cifar10/cifar1.jpg"],
+    category: ["ml & ai","featured"],
+    video: "/assets/gym_mobile.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["Python","Keras","TensorFlow"],
+    features:["Trains With Data","Multiple Classifier Methods","Bayesian","Performance analysis"]
+  },
+  {
+    id: 13,
+    title: "Mini Web project collection",
+    images: [
+      "./folio2/assets/projects/mini_projects/ez1.png",
+      "./folio2/assets/projects/mini_projects/ez2.gif",
+      "./folio2/assets/projects/mini_projects/ez3.gif",
+      "./folio2/assets/projects/mini_projects/ez4.gif",
+      "./folio2/assets/projects/mini_projects/ez5.gif",
+    ],
+    category: ["web","featured"],
+    video: "/assets/gym_mobile.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["JavaScript","React","HTML"],
+    features:["Lots Of Tiny Projects","Careful State Design","API","Axios"]
+  },
+  {
     id: 1,
     images: ["./folio2/assets/toni/logo.png"],
     title: "Portfolio Project",
-    category: ["web","app"],
+    category: ["web","featured"],
     description:"This is one my latest works, a highly interactive personal website that multitude of features. Designed with care, it has unobtrusive animations that lend a hint of charm to the browsing experience. As you navigate the simple layout, I hope you find something of interest.",
     tags:["JavaScript","React","UI/UX"],
     features:["Asymptotic Efficiecny","Optimized","Adaptive Algorithms"]
   },
   {
     id: 2,
-    title: "Tetris+",
+    title: "Tetris++",
     images: ["./folio2/assets/projects/misc/tetris.png"],
-    category: ["app","ml"],
+    category: ["other","featured"],
     video: "/assets/camping.mp4",
     description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-    tags:["C++"],
+    tags:["C++","Qtcreator"],
     features:["Asymptotic Efficiecny","Optimized","Adaptive Algorithms"]
   },
-  // {
-  //   id: 3,
-  //   title: "FUZU 3D Mobile Game",
-  //   images: ["./folio2/assets/projects/fuzu/fuzu1.png",
-  //   "./folio2/assets/projects/fuzu/fuzu2.gif",
-  //   "./folio2/assets/projects/fuzu/fuzu3.jpg",
-  //   "./folio2/assets/projects/fuzu/fuzu4.jpg",
-  //   "./folio2/assets/projects/fuzu/fuzu5.jpg",
-  // ],
-  //   category: "App",
-  //   video: "/assets/CloneYoutube.mkv",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["C#","Unity"],
-  //   features:["3D Platformer","Beautiful lvl Design","Skill tree","Highly Optimized"]
-  // },
-  // {
-  //   id: 4,
-  //   title: "Pirkanmaa Domination",
-  //   images: ["./folio2/assets/projects/misc/pirkanmaan_valloitus.png",],
-  //   category: "App",
-  //   video: "/assets/gym.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["C++","Pair Programming"],
-  //   features:["Multiplayer","Random-Gen map","5 Unique resources"]
-  // },
-  // {
-  //   id: 5,
-  //   title: "Suez Unblocker",
-  //   images: ["./folio2/assets/projects/suez/suez1.png","./folio2/assets/projects/suez/suez2.png","./folio2/assets/projects/suez/suez3.png"],
-  //   category: "App",
-  //   video: "/assets/movie.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["C#","Unity"],
-  //   features:["Clicker Game","Trending","Immersive Audio design"]
-  // },
-  // {
-  //   id: 6,
-  //   title: "Cyberpunk: Rogue Turret",
-  //   images: ["./folio2/assets/projects/cyberpunk/cyber1.png",
-  //   "./folio2/assets/projects/cyberpunk/cyber2.png",
-  //   "./folio2/assets/projects/cyberpunk/cyber3.png",
-  //   "./folio2/assets/projects/cyberpunk/cyber4.png",
-  //   "./folio2/assets/projects/cyberpunk/cyber5.png",
-  // ],
-  //   category: "App",
-  //   video: "/assets/todo.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["C#","Unity"],
-  //   features:["Highly Optimized","Highly Randomized","Stunning Visuals","40% D1 Retention"]
-  // },
+  {
+    id: 3,
+    title: "FUZU 3D Mobile Game",
+    images: ["./folio2/assets/projects/fuzu/fuzu1.png",
+    "./folio2/assets/projects/fuzu/fuzu2.gif",
+    "./folio2/assets/projects/fuzu/fuzu3.jpg",
+    "./folio2/assets/projects/fuzu/fuzu4.jpg",
+    "./folio2/assets/projects/fuzu/fuzu5.jpg",
+  ],
+  category: ["app","featured"],
+  video: "/assets/CloneYoutube.mkv",
+  description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+  tags:["C#","Unity"],
+  features:["3D Platformer","Beautiful lvl Design","Skill tree","Highly Optimized"]
+},
+  {
+    id: 4,
+    title: "Pirkanmaa Domination",
+    images: ["./folio2/assets/projects/misc/pirkanmaan_valloitus.png",],
+    category: ["other"],
+    video: "/assets/gym.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["C++","Pair Programming"],
+    features:["Multiplayer","Random-Gen map","5 Unique resources"]
+  },
 
-  // {
-  //   id: 7,
-  //   title: "Juice Master",
-  //   images: ["./folio2/assets/projects/juice/juice0.png",
-  //   "./folio2/assets/projects/juice/juice1.png",
-  //   "./folio2/assets/projects/juice/juice2.png",
-  //   "./folio2/assets/projects/juice/juice3.png",
-  //   "./folio2/assets/projects/juice/juice4.png",
-  // ],
-  //   category: "app",
-  //   video: "/assets/gym_mobile.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["C#","Unity"],
-  //   features:["Create Luxurious Drinks","Cool Animated processes","Make money with your drinks"]
-  // },
-  // {
-  //   id: 8,
-  //   title: "Tampere Bus Algorithms",
-  //   images: ["./folio2/assets/projects/misc/bus.png","./folio2/assets/projects/misc/bus2.png"],
-  //   category: "app",
-  //   video: "/assets/gym_mobile.mp4",
-  //   description: "Optimized Data Handling using unordered_maps selected for efficient handling of frequent functions achieving O(N) insertion, removal, and Θ(1) search. used vectors for less critical tasks due to their user-friendly nature.",
-  //   tags:["C++","Optimization"],
-  //   features:["Asymptotic Efficiecny","Optimized","Adaptive Algorithms"]
-  // },
-  // {
-  //   id: 10,
-  //   title: "Image Recognition",
-  //   images: ["./folio2/assets/projects/cifar10/cifar2.png","./folio2/assets/projects/cifar10/cifar1.jpg"],
-  //   category: "app",
-  //   video: "/assets/gym_mobile.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["Python","Keras","TensorFlow"],
-  //   features:["Trains With Data","Multiple Classifier Methods","Bayesian","Performance analysis"]
-  // },
-  // {
-  //   id: 11,
-  //   title: "AR/VR Augmented Food",
-  //   images: ["./folio2/assets/projects/ar/ar0.png",
-  //   "./folio2/assets/projects/ar/ar1.gif",
-  //   "./folio2/assets/projects/ar/ar2.png",
-  //   "./folio2/assets/projects/ar/ar3.png",
-  // ],
-  //   category: "app",
-  //   video: "/assets/gym_mobile.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["C#","VR","AR"],
-  //   features:["Augment Food","Real-Time Application","Calibration & Testing"]
-  // },
-  // {
-  //   id: 12,
-  //   title: "Ninja Blog",
-  //   images: ["./folio2/assets/projects/ninja_blog/blog0.png",
-  //   "./folio2/assets/projects/ninja_blog/blog1.gif",
-  //   "./folio2/assets/projects/ninja_blog/blog2.gif"],
-  //   category: "web",
-  //   video: "/assets/gym_mobile.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["JavaScript","React","Axios"],
-  //   features:["Write Blogs","Create and Store New ones","Minimalistic Design"]
-  // },
-  // {
-  //   id: 13,
-  //   title: "Mini Web project collection",
-  //   images: [
-  //     "./folio2/assets/projects/mini_projects/ez1.png",
-  //     "./folio2/assets/projects/mini_projects/ez2.gif",
-  //     "./folio2/assets/projects/mini_projects/ez3.gif",
-  //     "./folio2/assets/projects/mini_projects/ez4.gif",
-  //     "./folio2/assets/projects/mini_projects/ez5.gif",
-  //   ],
-  //   category: "featured",
-  //   video: "/assets/gym_mobile.mp4",
-  //   description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
-  //   tags:["JavaScript","React","HTML"],
-  //   features:["Lots Of Tiny Projects","Careful State Design","API","Axios"]
-  // },
+  {
+    id: 6,
+    title: "Cyberpunk: Rogue Turret",
+    images: ["./folio2/assets/projects/cyberpunk/cyber1.png",
+    "./folio2/assets/projects/cyberpunk/cyber2.png",
+    "./folio2/assets/projects/cyberpunk/cyber3.png",
+    "./folio2/assets/projects/cyberpunk/cyber4.png",
+    "./folio2/assets/projects/cyberpunk/cyber5.png",
+  ],
+  category: ["app","featured"],
+    video: "/assets/todo.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["C#","Unity"],
+    features:["Highly Optimized","Highly Randomized","Stunning Visuals","40% D1 Retention"]
+  },
+
+  {
+    id: 7,
+    title: "Juice Master",
+    images: ["./folio2/assets/projects/juice/juice0.png",
+    "./folio2/assets/projects/juice/juice1.png",
+    "./folio2/assets/projects/juice/juice2.png",
+    "./folio2/assets/projects/juice/juice3.png",
+    "./folio2/assets/projects/juice/juice4.png",
+  ],
+  category: ["app","featured"],
+    video: "/assets/gym_mobile.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["C#","Unity"],
+    features:["Create Luxurious Drinks","Cool Animated processes","Make money with your drinks"]
+  },
+  {
+    id: 8,
+    title: "Tampere Bus Algorithms",
+    images: ["./folio2/assets/projects/misc/bus.png","./folio2/assets/projects/misc/bus2.png"],
+    category: ["other"],
+    video: "/assets/gym_mobile.mp4",
+    description: "Optimized Data Handling using unordered_maps selected for efficient handling of frequent functions achieving O(N) insertion, removal, and Θ(1) search. used vectors for less critical tasks due to their user-friendly nature.",
+    tags:["C++","Optimization"],
+    features:["Asymptotic Efficiecny","Optimized","Adaptive Algorithms"]
+  },
+ 
+  
+  {
+    id: 12,
+    title: "Ninja Blog",
+    images: ["./folio2/assets/projects/ninja_blog/blog0.png",
+    "./folio2/assets/projects/ninja_blog/blog1.gif",
+    "./folio2/assets/projects/ninja_blog/blog2.gif"],
+    category: ["web","featured"],
+    video: "/assets/gym_mobile.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["JavaScript","React","Axios"],
+    features:["Write Blogs","Create and Store New ones","Minimalistic Design"]
+  },
+  {
+    id: 5,
+    title: "Suez Unblocker",
+    images: ["./folio2/assets/projects/suez/suez1.png","./folio2/assets/projects/suez/suez2.png","./folio2/assets/projects/suez/suez3.png"],
+    category: ["app"],
+    video: "/assets/movie.mp4",
+    description: "Description is the text that do go here, idk if it should be 2 or 3 lines. but probably three i would say.",
+    tags:["C#","Unity"],
+    features:["Clicker Game","Trending","Immersive Audio design"]
+  },
 
 ];
 
@@ -162,6 +165,9 @@ export const projectsNav = [
   },
   {
     name: "ML & AI",
+  },
+  {
+    name: "other",
   },
   {
     name: "all",
