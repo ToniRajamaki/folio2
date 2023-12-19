99eaa9e54c7d18c01deac154d57b462f7aef49af
TONI <zingoo16@gmail.com>
2023-12-19 14:10:59 +0200

    removed some social links from footer and changed CV downlaod asset

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,39 +21,10 @@ const Footer = ({ theme }) => {
 
           <li>
             <a href="#testimonials" className="footer__link">
-              Testimonials
+              Education
             </a>
           </li>
         </ul>
-
-        <div className="footer__social">
-          <a
-            href="https://www.facebook.com/hazem.pansho/"
-            className="footer__social-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bxl-facebook"></i>
-          </a>
-
-          <a
-            href="https://www.instagram.com/hazemfarhati/"
-            className="footer__social-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bxl-instagram"></i>
-          </a>
-
-          <a
-            href="https://twitter.com/HazemFarhati"
-            className="footer__social-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bxl-twitter"></i>
-          </a>
-        </div>
 
         <span className="footer__copy">© 2023, Toni Rajamäki </span>
       </div>
