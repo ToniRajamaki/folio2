eb342ed5bbcba88c0d024fdcc2f973fcb28b5a57
TONI <zingoo16@gmail.com>
2024-03-26 12:52:12 +0200

    removed animatedNumbers and opted to data-aos

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,111 +1,67 @@
 import React from "react";
-import AnimatedNumbers from "react-animated-numbers";
 
 const Info = React.memo(() => {
   return (
     <div className="about__info center container">
-
-    <div className="about__box">
-      <div className="flex">
-
-             <h3 className="about__title">  
-            <AnimatedNumbers
-            includeComma
-            transitions={(index) => ({
-              type: 'spring',
-              duration: 0.5 + 0.4,
-            })}
-            fontStyle={{
-              fontSize: "3em",
-              fontWeight: "500",
-              color: "var(--main)",
-            }}
-            animateToNumber={2}
-            />
-            </h3>
-          <span className="sliding-number-postfix">+</span>
-
-            </div>
-            <p className="about__subtitle">Years of Experience</p>
-          </div>
-    
-    
-          <div className="about__box">
-            <div className="flex">
-
-             <h3 className="about__title">  
-            <AnimatedNumbers
-            includeComma
-            transitions={(index) => ({
-              type: 'spring',
-              // stiffness:200,
-              duration: 0.5 + 0.4,
-            })}
-            fontStyle={{
-              fontSize: "3em",
-              fontWeight: "500",
-              color: "var(--main)",
-            }}
-            animateToNumber={30}
-            />
-            </h3>
-          <span className="sliding-number-postfix">+</span>
-
-            </div>
-            <p className="about__subtitle">Projects Completed</p>
-          </div>
-    
-    
-          <div className="about__box">
-            <div className="flex">
-              
-             <h3 className="about__title">  
-            <AnimatedNumbers
-            includeComma
-            transitions={(index) => ({
-              type: 'spring',
-              // stiffness:200,
-              duration: 0.5 + 0.4,
-            })}
-            fontStyle={{
-              fontSize: "3em",
-              fontWeight: "500",
-              color: "var(--main)",
-            }}
-            animateToNumber={2500}
-            />
-            </h3>
-          <span className="sliding-number-postfix">+</span>
-            
-            </div>
-            <p className="about__subtitle">Total Commits</p>
-          </div>
-    
-    
-          <div className="about__box">
-    
-             <h3 className="about__title">  
-            <AnimatedNumbers
-            includeComma = {false}
-            transitions={(index) => ({
-              type: 'spring',
-              // stiffness:200,
-              duration: 0.5 + 0.4,
-            })}
-            fontStyle={{
-              fontSize: "3em",
-              fontWeight: "500",  
-              color: "var(--main)",
-            }}
-            animateToNumber={2017}
-            />
-            </h3>
-            <p className="about__subtitle">Coding since</p>
-          </div>
-    
-       
+      <div className="about__box">
+        <div className="flex">
+          <h3 className="about__title" data-aos="fade-down" data-aos-delay="200" style={{
+            fontSize: "2.5em",
+            fontWeight: "500",
+            color: "#071013",
+          }}>
+            2+
+          </h3>
         </div>
+        <p className="about__subtitle da" data-aos="fade-up" data-aos-delay="200">Years of Experience</p>
+      </div>
+
+      <div className="about__box">
+        <div className="flex">
+          <h3 className="about__title" data-aos="fade-down" data-aos-delay="400" style={{
+            fontSize: "2.5em",
+            fontWeight: "500",
+            color: "#071013",
+          }}>
+            30+
+          </h3>
+        </div>
+        <p className="about__subtitle da" data-aos="fade-up" data-aos-delay="400">Projects Completed</p>
+      </div>
+
+      <div className="about__box">
+        <div className="flex">
+          <h3 className="about__title" data-aos="fade-down" data-aos-delay="600" style={{
+            fontSize: "2.5em",
+            fontWeight: "500",
+            color: "#071013",
+          }}>
+            2500+
+          </h3>
+        </div>
+        <p className="about__subtitle da" data-aos="fade-up" data-aos-delay="600">Total Commits</p>
+      </div>
+
+      <div className="about__box">
+        <div className="flex">
+          <h3 className="about__title" data-aos="fade-down" data-aos-delay="800" style={{
+            fontSize: "2.5em",
+            fontWeight: "500",
+            color: "#071013",
+          }}>
+            2017
+          </h3>
+        </div>
+        <p className="about__subtitle da" data-aos="fade-up" data-aos-delay="800">Coding since</p>
+      </div>
+    </div>
   );
 });
 
 export default Info;
+
+// Ensure AOS is initialized, possibly in your component's useEffect hook or in the main entry file of your app.
+// Example of AOS initialization:
+// useEffect(() => {
+//   AOS.init();
+// }, []);
